a46093f2d2f20e16e78eb8c7e3c4e72558fa8a0e
Kenneth Newman <kennethnewman80@gmail.com>
2021-11-02 18:20:24 -0500

    created and completed the create book component

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Footer.css'
+
+
+function Footer(props){
+    return(
+        <div className="row row-space">
+            <div className="col-md-12" style={{padding: 0}}>
+                 <div className="footer-nav">
+                    <h1>Footer</h1>      
+                 </div>
+                 
+             </div>       
+        </div>
+    )
+}
+
+export default Footer;
